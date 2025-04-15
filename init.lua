8aa904aa707f8b42405ebcec5d6af6760913518d
Tanarat <tanarats7717@gmail.com>
2025-04-15 22:21:07 +0700

    fix: change colorscheme

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("plugins.custom-plugins")
-vim.cmd("colorscheme nordern")
+require("plugins.colorscheme")
+vim.cmd("colorscheme tokyonight")
